e6a4614416904ac73e356e3a6ad25e1a9b465d0e
acrighthere <rodionovmaksprog@mail.ru>
2026-01-06 23:51:58 +0200

    Product-service mvp added

---
+++ product-service/src/main/java/com/acrighthere/product_service/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.acrighthere.product_service.service;
+
+import com.acrighthere.product_service.dto.ProductRequest;
+import com.acrighthere.product_service.dto.ProductResponse;
+import com.acrighthere.product_service.model.Product;
+import com.acrighthere.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product {} with id:{} created", product.getName(), product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
