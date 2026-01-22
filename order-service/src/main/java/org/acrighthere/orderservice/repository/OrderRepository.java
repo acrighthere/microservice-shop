98385bd8d7d476542cd70aa2f8779d9f061a26d7
acrighthere <rodionovmaksprog@mail.ru>
2026-01-22 19:08:10 +0300

    Rethinking of stack

--- order-service/src/main/java/org/acrighthere/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package org.acrighthere.orderservice.repository;
-
-import org.acrighthere.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
