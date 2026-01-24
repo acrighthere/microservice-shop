a0ffc0880ac091c863b43a8fd3784b7d093ad2be
acrighthere <rodionovmaksprog@mail.ru>
2026-01-24 20:34:51 +0300

    Api gateway added

---
+++ api-gateway/src/main/java/org/acrighthere/gateway/routes/Routes.java
@@ -0,0 +1,41 @@
+package org.acrighthere.gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
+
+import java.net.URI;
+
+@Configuration
+public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute(){
+        return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8080")))
+                .build();
+
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute(){
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
+                .build();
+
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute(){
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8082")))
+                .build();
+
+    }
+
+
+}
