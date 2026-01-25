d769dcac7af2448694be1e455e61291d74baf379
acrighthere <rodionovmaksprog@mail.ru>
2026-01-25 19:40:36 +0300

    Aggregate API information via gateway

--- api-gateway/src/main/java/org/acrighthere/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/acrighthere/gateway/config/SecurityConfig.java
@@ -8,10 +8,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String[] freeResourceUrls = {
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v3/api-docs/**",
+            "/api-docs/**",
+            "/aggregate/**",
+            "/swagger-resources/**",
+            "/webjars/**"
+    };
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(csrf -> csrf.ignoringRequestMatchers(freeResourceUrls))
                 .build();
     }
 }
--- api-gateway/src/main/java/org/acrighthere/gateway/routes/Routes.java
+++ api-gateway/src/main/java/org/acrighthere/gateway/routes/Routes.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.function.*;
 
 import java.net.URI;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
     @Bean
@@ -36,6 +38,35 @@ public class Routes {
                 .build();
 
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/api-docs"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8080")))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/api-docs"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/api-docs"), HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8082")))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+
 
 
 }
